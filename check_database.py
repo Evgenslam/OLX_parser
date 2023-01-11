b42aa41c43ade34902d11b60edc86e2664e3919a
EvgenSlam <evgenyb86@gmail.com>
2023-01-11 17:00:46 +0300

    Update requirements and comments in other files

--- check_database.py
+++ check_database.py
@@ -1,9 +1,17 @@
-'''
-Функция проверяет, есть ли объява в БД, если нет, а) присылает её в телегу б) добавляет в БД в)делает принты
-'''
+import sqlite3
+import time
+
+from get_offer import get_offer
+from send_telegram import send_telegram
+
+start_time: float = time.time()
 
 
 def check_database(card: str) -> None:
+    '''
+    This function checks if the ad is already in the DB. If it is not, a)sends in to Telegram b)adds it to the DB
+    c)makes necessary prints
+    '''
     title: str = card.find('h6').text
     with sqlite3.connect('DB/realty4.db') as connection:
         cursor = connection.cursor()
