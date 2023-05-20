a1ae64cb93e323be4d233e63418fa703e55e86cb
EvgenSlam <evgenyb86@gmail.com>
2023-05-20 12:54:22 +0300

    remove storage from arguments of Dispatcher in loader.py

--- loader.py
+++ loader.py
@@ -1,12 +1,12 @@
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.memory import MemoryStorage # Might be absent in the current version
 from decouple import config
 from database import Database
 
 
-storage: MemoryStorage = MemoryStorage() # TODO: how do we use Memory storage here?
+#storage: MemoryStorage = MemoryStorage() # TODO: how do we use Memory storage here?
 bot = Bot(token=config('bot_token'))
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher() #(storage=storage)
 db = Database(db_path='DB/realty5.db')
 
 db.del_db_content()
